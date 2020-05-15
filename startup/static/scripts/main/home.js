b43d319f1318ce485ed532ef72a07da8b6eb5001
Solinx <firstkillinbattle@gmail.com>
2020-05-16 00:48:13 +0300

    Fixed UI
    Fixed UI

--- startup/static/scripts/main/home.js
+++ startup/static/scripts/main/home.js
@@ -2,14 +2,29 @@ function fullScreen(dataValue) {
     let fullScreenCardElement = document.querySelector(".full-screen-card");
 
     if (fullScreenCardElement.children[0] != undefined) {
+        let fullScreenElement = document.querySelector(".information-about-project__major-content_full-screen");
+        let targetDataValue = +fullScreenElement.getAttribute("data-value");
         fullScreenCardElement.classList.remove("active");
         fullScreenCardElement.removeChild(fullScreenCardElement.children[0]);
-        cardMajors[dataValue].classList.add("active");
+        cardMajors[targetDataValue].classList.add("active");
     } else {
-        
-        ajaxQuery("GET", "/fullScreenCard/", undefined, undefined, ".full-screen-card");
+        callbackAjax.complete.func = setFullScreenDaat;
+        callbackAjax.complete.params = [dataValue];
+        ajaxQueryCalback("GET", "/fullScreenCard/", undefined, undefined, undefined, ".full-screen-card", callbackAjax);
 
         cardMajors[dataValue].classList.remove("active");
         fullScreenCardElement.classList.add("active");
     }
+}
+
+function setFullScreenDaat(dataValue) {
+    let informAboutProject = document.querySelectorAll(".information-about-project__major-content");
+    let imgForFullScreen = document.querySelector(".img-for-full-screen");
+    let imgMajorCard = informAboutProject[dataValue].children[1].cloneNode(true);
+    let lastChild = informAboutProject[dataValue].lastElementChild.cloneNode(true);
+    let fullScreenElement = document.querySelector(".information-about-project__major-content_full-screen");
+    fullScreenElement.setAttribute("data-value", dataValue);
+
+    imgForFullScreen.appendChild(imgMajorCard);
+    fullScreenElement.appendChild(lastChild);
 }
